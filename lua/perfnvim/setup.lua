d29d86409187d4177a707059b8d0535d37d39af6
Christenson, Johannes <johannes.christenson@frostbite.com>
2026-02-25 12:24:55 +0100

    Adding support for using mini.diff take 2

--- lua/perfnvim/setup.lua
+++ lua/perfnvim/setup.lua
@@ -4,26 +4,6 @@ local constants = require("perfnvim.constants")
 local change_helpers = require("perfnvim.helpers.change_helpers")
 
 local function setup()
-	local green = vim.api.nvim_get_hl(0, { name = "GitSignsAdd" }).fg
-	vim.api.nvim_set_hl(0, constants.p4addSignHighlight, { fg = green, bg = "NONE" })
-	vim.fn.sign_define(constants.p4addSignName, {
-		text = "▐",
-		texthl = constants.p4addSignHighlight,
-	})
-
-	local yellow = vim.api.nvim_get_hl(0, { name = "GitSignsChange" }).fg
-	vim.api.nvim_set_hl(0, constants.p4changeSignHighlight, { fg = yellow, bg = "NONE" })
-	vim.fn.sign_define(constants.p4changeSignName, {
-		text = "▐",
-		texthl = constants.p4changeSignHighlight,
-	})
-
-	local red = vim.api.nvim_get_hl(0, { name = "GitSignsDelete" }).fg
-	vim.api.nvim_set_hl(0, constants.p4deleteSignHighlight, { fg = red, bg = "NONE" })
-	vim.fn.sign_define(constants.p4deleteSignName, {
-		text = "▐",
-		texthl = constants.p4deleteSignHighlight,
-	})
 	vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
 		pattern = "*",
 		callback = change_helpers._AnnotateSigns,
